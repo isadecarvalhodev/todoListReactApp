668a846df2148b516a2a0e729c2e0163e8b3215c
Isa Carvalho <isadecarvalhodev@gmail.com>
2023-11-24 02:39:55 +0000

    add todo Logic

--- todolistapp/src/App.js
+++ todolistapp/src/App.js
@@ -6,14 +6,19 @@ import TodoList from './componets/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState("");
-  
+  const [todos, setTodos] = useState([]);
+
+
   return (
     <div className="App">
       <header>
         <h1>My Todo List</h1>
       </header>
-      <Form/>
-      <TodoList />
+      <Form inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}/>
+      <TodoList todos={todos} />
       
     </div>
   );
--- todolistapp/src/componets/TodoList.js
+++ todolistapp/src/componets/TodoList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+
     return (
         <div className="todo-container">
-            <ul className="todo-lis">
+            <ul className="todo-list">
+                {todos.map((todo) => (
+                    <Todo text={todo.text}/>
+                ))}
+                
             </ul>
         </div>
     );
