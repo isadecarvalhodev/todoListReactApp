7487b6d9e47f2cff4ad14940928eade042ee7258
Isa Carvalho <isadecarvalhodev@gmail.com>
2023-11-24 02:55:09 +0000

    add delete button

--- todolistapp/src/componets/Todo.js
+++ todolistapp/src/componets/Todo.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-const Todo = ({text}) => {
+const Todo = ({text, todo, todos, setTodos}) => {
+    //Events
+    const deleteHandler = () => {
+       setTodos(todos.filter(el => el.id !== todo.id));
+
+    };
     return (
         <div className="todo">
             <li className="todo-item">{text}</li>
             <button className="complete-btn">
                 <i className="fa fa-check"></i>
             </button>
-            <button className="trash-btn">
+            <button onClick={deleteHandler} className="trash-btn">
                 <i className="fa fa-trash"></i>
             </button>
 
--- todolistapp/src/componets/TodoList.js
+++ todolistapp/src/componets/TodoList.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
 
     return (
         <div className="todo-container">
             <ul className="todo-list">
                 {todos.map((todo) => (
-                    <Todo text={todo.text}/>
+                    <Todo 
+                    setTodos={setTodos}
+                    todos={todos} 
+                     key={todo.id} 
+                     todo={todo}
+                    text={todo.text}/>
                 ))}
                 
             </ul>
